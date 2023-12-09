007df4237ee2dc58e933979621c05386fe853d3e
benjirer <80070426+benjirer@users.noreply.github.com>
2023-12-09 22:22:03 +0100

    fix ?

--- src/netunicorn/director/infrastructure/connectors/aws.py
+++ src/netunicorn/director/infrastructure/connectors/aws.py
@@ -317,21 +317,21 @@ class AWSFargate(NetunicornConnectorProtocol):
                     {"name": key, "value": value}
                     for key, value in deployment.environment_definition.runtime_context.environment_variables.items()
                 ],
-                # "portMappings": [
-                #     {
-                #         "containerPort": container_port,
-                #         "hostPort": host_port,
-                #         "protocol": "tcp"
-                #     }
-                #     for container_port, host_port in deployment.environment_definition.runtime_context.ports_mapping.items()
-                # ],
                 "portMappings": [
                     {
-                        "containerPort": 8080,
-                        "hostPort": 8080,
+                        "containerPort": container_port,
+                        "hostPort": host_port,
                         "protocol": "tcp"
-                    },
+                    }
+                    for container_port, host_port in deployment.environment_definition.runtime_context.ports_mapping.items()
                 ],
+                # "portMappings": [
+                #     {
+                #         "containerPort": 8080,
+                #         "hostPort": 8080,
+                #         "protocol": "tcp"
+                #     },
+                # ],
             }
 
             parameters = {
